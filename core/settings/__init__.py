10efe16b9ac4028783c3848398213179f346f6ee
arnazdj <arnazdj@gmail.com>
2025-09-06 02:11:40 +0800

    reverted

--- core/settings/__init__.py
+++ core/settings/__init__.py
@@ -1,14 +1,1 @@
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
-
-
-environment = os.getenv("ENVIRONMENT")
-print(f"Current Environment: {environment}")
-if environment == "development":
-    from .dev import *
-elif environment == "production":
-    from .prod import *
-else:
-    from .base import *
+from .base import *
