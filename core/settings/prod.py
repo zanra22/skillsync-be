31075a78104d14b598493078d6946f2325497cb2
arnazdj <arnazdj@gmail.com>
2025-09-05 20:57:26 +0800

    feat: Updated prod settings

--- core/settings/prod.py
+++ core/settings/prod.py
@@ -1,11 +1,11 @@
 # core/settings/prod.py
 from .base import *
-from config.constants import DATABASES, CORS_CONFIG, ALLOWED_HOSTS, FRONTEND_URL
+from config.constants import DATABASES, FRONTEND_URL
 import os
 
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['.azurewebsites.net', '127.0.0.1', 'localhost', '.skillsync.studio']
 
 FRONTEND_URL = FRONTEND_URL["production"]
 
